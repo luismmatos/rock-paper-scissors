c880e275ac9dd2028ba3a200b42aaa4ab31bcc4e
luismmatos <118832995+luismmatos@users.noreply.github.com>
2025-08-13 17:26:49 +0100

    Add playRound function

--- game.js
+++ game.js
@@ -15,3 +15,35 @@ function getHumanChoice() {
     return choice;
 };
 
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    const human = humanChoice.toLowerCase();
+
+    if (human === computerChoice) {
+        return "Draw!!"
+    } else if (
+        (human === "rock" && computerChoice === "scissors") ||
+        (human === "paper" && computerChoice === "rock") ||
+        (human === "scissors" && computerChoice === "paper")
+    ) {
+        humanScore++;
+        return `You Win!! ${human} beats ${computerChoice}`;
+    } else {
+        computerScore++;
+        return `You Lose!! ${computerChoice} beats ${human}`;
+    }
+};
+
+
+
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+
+
+console.log("The player chose: ", humanChoice);
+console.log("The computer chose: ", computerChoice);
+console.log(playRound(humanChoice, computerChoice));
+console.log(`Score: Humano ${humanScore} - ${computerScore} Computador`);
